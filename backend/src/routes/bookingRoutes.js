a8a834b72629266ee713b8a352733d9783b77b94
anuraj-2000 <anurajmunnad@gmail.com>
2026-03-18 13:56:16 +0530

    bookingRoutes bug fixed
    Uncommented the booking routes for express.

--- backend/src/routes/bookingRoutes.js
+++ backend/src/routes/bookingRoutes.js
@@ -1,14 +1,3 @@
-// import express from "express";
-
-// import { validateBooking } from "../middleware/validationMiddleware.js";
-// import { createBooking, getBookingsByShow } from "../controllers/bookingController.js";
-
-// const router = express.Router();
-
-// router.post("/", validateBooking, createBooking);
-// router.get("/:showId", getBookingsByShow);
-
-// export default router;
 
 import express from "express";
 
